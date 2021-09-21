5e5046bf4ffc9653cd319a7282309587636890ab
Prince-Thind <princethind799@gmail.com>
2021-09-21 12:23:03 +0530

    test commit for copy

---
+++ index.js
@@ -0,0 +1,6 @@
+function copy(text) {
+  navigator.clipboard.writeText(text);
+}
+
+const para='Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias error, necessitatibus molestiae pariatur quaerat ab magni consequuntur ex harum deserunt.'
+copy(para);
