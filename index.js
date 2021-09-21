8784b21209108ec9abbfa6817eeb057981a11793
Prince-Thind <princethind799@gmail.com>
2021-09-21 12:34:15 +0530

    cleaning up commit

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-function copy(text) {
-  navigator.clipboard.writeText(text);
-}
-
-const para='Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias error, necessitatibus molestiae pariatur quaerat ab magni consequuntur ex harum deserunt.'
-copy(para);
+console.log('heyo!');
